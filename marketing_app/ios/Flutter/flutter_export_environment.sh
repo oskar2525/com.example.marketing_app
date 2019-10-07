254e67bcbc7ac980658e36264a205a83b5fe8942
oskar2525 <56242972+oskar2525@users.noreply.github.com>
2019-10-07 10:50:11 +0700

    Add files via upload
    Projcet

---
+++ marketing_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\Sdk\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Tugas\PKL\Project\Flutter\Coba Di VS\marketing_app"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=E:\Sdk\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
